fc7f38f5fd96d2229c0493b3775ce8f09fc7e2bc
dimagerasimov <dimitrigerasimov24@gmail.com>
2023-11-12 11:00:32 -0500

    updated chrome options

--- utilities/driver_setup.py
+++ utilities/driver_setup.py
@@ -12,8 +12,8 @@ class WebDriverFactory:
         if browser_name == "chrome":
             options = webdriver.ChromeOptions()
             options.add_argument('--window-size=1920x1080')
-            # options.add_argument('--headless')
-            # options.add_argument('--disable-gpu')
+            options.add_argument('--headless')
+            options.add_argument('--disable-gpu')
             options.add_argument('--disable-dev-shm-usage')
             options.add_argument('--no-sandbox')
             options.add_argument('--disable-extensions')
@@ -23,6 +23,8 @@ class WebDriverFactory:
             options.add_argument('--disable-web-security')
             options.add_argument('--enable-logging')
             options.add_argument('--v=1')
+            options.add_argument("--proxy-server='direct://'");
+            options.add_argument("--proxy-bypass-list=*");
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
             options.page_load_strategy = 'normal'
