9fba39f511a301e90c3423b06c7ccf4db8900851
dimagerasimov <dimitrigerasimov24@gmail.com>
2023-11-12 11:01:45 -0500

    fixed typo

--- utilities/driver_setup.py
+++ utilities/driver_setup.py
@@ -23,7 +23,7 @@ class WebDriverFactory:
             options.add_argument('--disable-web-security')
             options.add_argument('--enable-logging')
             options.add_argument('--v=1')
-            options.add_argument("--proxy-server='direct://'");
+            options.add_argument("--proxy-server='direct://'")
             options.add_argument("--proxy-bypass-list=*");
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
