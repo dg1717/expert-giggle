bedc178fa568771e7a448785b28fd0ee82283106
dimagerasimov <dimitrigerasimov24@gmail.com>
2023-12-20 18:44:41 -0500

    updated project based on feedback

--- features/environment.py
+++ features/environment.py
@@ -1,19 +1,28 @@
 from utilities.driver_setup import WebDriverFactory
+from utilities.appium_setup import AppiumSetup
 
 
 def before_scenario(context, scenario):
-    # Initialize the WebDriver before each scenario
-    context.driver = WebDriverFactory.get_webdriver()
+    if "web" in scenario.tags:
+        # Initialize the WebDriver for web scenarios
+        context.driver = WebDriverFactory.get_webdriver()
+    elif "mobile" in scenario.tags:
+        # Initialize the Appium driver for mobile scenarios
+        context.appium_setup = AppiumSetup(platform_version="14.0")
+        context.appium_setup.start_driver()
+        context.driver = context.appium_setup.driver
 
 
 def after_scenario(context, scenario):
-    # Quit the WebDriver after each scenario
+    # Quit the appropriate driver after each scenario
     if hasattr(context, "driver"):
         context.driver.quit()
+    if "mobile" in scenario.tags and hasattr(context, "appium_setup"):
+        context.appium_setup.stop_driver()
 
 
 def after_step(context, step):
     if step.status == "failed":
-        # Take a screenshot or perform some other error handling/logging
-        # Ensure that the directory for the screenshots exists or handle the FileNotFoundError
+        # Handle screenshot taking for both web and mobile
+        # Ensure the appropriate directory for screenshots exists
         context.driver.save_screenshot(f"screenshots/{step.name}.png")
--- features/hotel_price.feature
+++ features/hotel_price.feature
@@ -4,8 +4,8 @@ Feature: Hotel price monitoring for optimal booking
   I want to know the date in a month when the price of a specific hotel is the lowest
   So that I can book the hotel at the optimal price
 
-  @hotel
-  Scenario Outline: Identify the date with the lowest price for The Charleston Place in a month
+  @hotel @web
+  Scenario Outline: [Web] Identify the date with the lowest price for The Charleston Place in a month
     Given I am monitoring the price of the hotel per night for "<hotel_name>" for the next 30 days
     When I analyze the prices for each day
     Then I should identify the date with the lowest price
@@ -14,5 +14,15 @@ Feature: Hotel price monitoring for optimal booking
     Examples:
       | hotel_name           | desired_amount |
       | The Charleston Place | 400            |
-      #| French Quarter Inn   | 400            |
+
+#  @hotel @mobile
+#  Scenario Outline: [Mobile] Identify the date with the lowest price for The Charleston Place in a month
+#    Given I am monitoring the price of the hotel per night for "<hotel_name>" for the next 30 days on my mobile device
+#    When I analyze the prices for each day on the mobile app
+#    Then I should identify the date with the lowest price
+#    And notify me if that price is below "<desired_amount>" on my mobile device
+#
+#    Examples:
+#      | hotel_name           | desired_amount |
+#      | The Charleston Place | 400            |
 
